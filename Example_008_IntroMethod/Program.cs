77007c7eda8ebe53bd110283f019416a8b835461
Garik <requie@mail.ru>
2022-12-08 19:48:36 +0700

    example 008 intromethod

---
+++ Example_008_IntroMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿// поиск максимального числа из 9 с помощью функции
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 125;
+int c1 = 10;
+int a2 = 33;
+int b2 = 57;
+int c2 = 41;
+int a3 = 345;
+int b3 = 71;
+int c3 = 68;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
